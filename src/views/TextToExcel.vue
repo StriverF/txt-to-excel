58deba584a7516d78e532ee0c624346f19f59190
尹锋 <striverf.yin@patpat.com>
2021-06-04 16:34:48 +0800

    feat: 新增字段

--- src/views/TextToExcel.vue
+++ src/views/TextToExcel.vue
@@ -70,44 +70,62 @@ export default {
     },
     exportTable () {
       const tableHeader = {
-        name: { title: '姓名', reg: /姓名：(\S*)\r/ },
-        gender: { title: '性别', reg: /性别：(\S*)\r/ },
-        age: { title: '年龄', reg: /年龄：(\S*)\r/ },
-        phone: { title: '电话', reg: /(手机1：(\S*)\r)|(电话：(\S*)\r)|(手机2：(\S*)\r)/ },
-        weight: { title: '体重', reg: /\n体重(\S*)\r/ },
-        adn: { title: '住院号', reg: /住院号：(\S*)\r/ },
-        nation: { title: '民族', reg: /民族：(\S*)\r/ },
-        nativePlace: { title: '籍贯', reg: /籍贯：(\S*)\r/ },
-        marriageStatus: { title: '婚姻状况', reg: /(婚姻状况： (\S*)\r)|(婚姻状况：(\S*)\r)/ },
-        inHospitalDate: { title: '住院日期', reg: /住院日期：(\S*)\r/ },
-        pulse: { title: '脉搏', reg: /\n脉搏(\S*)\r/ },
-        stature: { title: '身高', reg: / 身高(\S*)\r/ },
-        bp1: { title: '血压分子', reg: /\r\n右侧上肢血压([0-9]{1,4})/ },
-        bp2: { title: '血压分母', reg: /([0-9]{1,4}mmHg)\r/ },
-        mainArt: { title: '主术', reg: /主 诉： (\S*)\r/ },
-        // operationName: { title: '手术名称', reg: /r/ },
-        // operationDate: { title: '手术时间', reg: /r/ },
-        // operationPhysician: { title: '手术医师', reg: /r/ },
-        // operationResult: { title: '手术结果', reg: /r/ },
-        discharged: { title: '出院时间', reg: /出院时间:(\S*)\r|出院时间：(\S*)\r/ }
+        name: { title: '姓名', regs: [/姓名[:,：](\S*)\r/] },
+        gender: { title: '性别', regs: [/性别[:,：](\S*)\r/] },
+        age: { title: '年龄', regs: [/年龄[:,：](\S*)\r/] },
+        phone: { title: '电话', regs: [/手机1[:,：](\S*)\r/, /手机2[:,：](\S*)\r/, /电话[:,：](\S*)\r/] },
+        weight: { title: '体重', regs: [/\n体重(\S*)\r/] },
+        adn: { title: '住院号', regs: [/住院号[:,：](\S*)\r/] },
+        nation: { title: '民族', regs: [/民族[:,：](\S*)\r/] },
+        nativePlace: { title: '籍贯', regs: [/籍贯[:,：](\S*)\r/] },
+        marriageStatus: { title: '婚姻状况', regs: [/婚姻状况[:,：](\S*)\r/] },
+        inHospitalDate: { title: '住院日期', regs: [/住院日期[:,：](\S*)\r/] },
+        pulse: { title: '脉搏', regs: [/\n脉搏(\S*)\r/] },
+        stature: { title: '身高', regs: [/身高(\S*)\r/] },
+        bp1: { title: '血压分子', regs: [/\r\n右侧上肢血压([0-9]{1,4})/] },
+        bp2: { title: '血压分母', regs: [/([0-9]{1,4}mmHg)\r/] },
+        mainArt: { title: '主术', regs: [/主诉[:,：](\S*)\r/] },
+        historyPI: { title: '现病史', regs: [/现病史[:,：](\S*)\r/] },
+        historyITP: { title: '既往史', regs: [/既往史[:,：]([\s\S]*)\n个人史/] },
+        historyPersonal: { title: '个人史', regs: [/个人史[:,：]([\s\S]*)\n婚姻史/] },
+        historyHypertension: { title: '高血压病史', regs: [/\n(\S*高血压病史\S*)\r/] },
+        historyCI: { title: '脑梗病史', regs: [/\n(\S*脑梗病史\S*)\r/] },
+        historyHI: { title: '高血脂病史', regs: [/\n(\S*高血脂病史\S*)\r/] },
+        historyDiabetes: { title: '糖尿病病史', regs: [/\n(\S*糖尿病病史\S*)\r/] },
+        historyThyroid: { title: '甲状腺', regs: [/\n(\S*甲状腺\S*)\r/] },
+        historyAnemia: { title: '贫血', regs: [/\n(\S*贫血\S*)\r/] },
+        historySI: { title: '自身免疫', regs: [/\n(\S*自身免疫\S*)\r/] },
+        DSA_CTA_MRA: { title: 'DSA_CTA_MRA', regs: [/\n(\S*[DSA,CTA,MRA]\S*)\r/] },
+        initialDiagnosis: { title: '初步诊断', regs: [/初步诊断[:,：]([\s\S]*)\n住院医师/] },
+        operationName: { title: '手术名称', regs: [/手术名称[:,：](\S*)\r/] },
+        operationDate: { title: '手术时间', regs: [/手术时间[:,：](\S*)\r/] },
+        operationPhysician: { title: '手术医师', regs: [/手术医师[:,：]([\s\S]*)\r/, /手术医师1[:,：]([\s\S]*)\r/] },
+        operationResult: { title: '手术结果', regs: [/手术结果[:,：](\S*)\r/] },
+        discharged: { title: '出院时间', regs: [/出院时间[:,：](\S*)\r/] }
       }
       console.log(tableHeader)
       this.getTextArray().then(textArray => {
         const xlsxArray = []
         textArray.forEach((text, index) => {
+          text = text.replace(/ /g, '')
           const item = {}
           for (const key in tableHeader) {
             if (Object.hasOwnProperty.call(tableHeader, key)) {
               const element = tableHeader[key]
               // console.log(element.title, regExecRes)
-              let matchRes = text.match(element.reg)
-              if (matchRes) {
-                matchRes = matchRes.filter(item => item && item.indexOf('\r') < 0)
-                item[element.title] = matchRes[0]
-              } else {
-                item[element.title] = ''
+              for (let index = 0; index < element.regs.length; index++) {
+                const reg = element.regs[index]
+                const matchRes = text.match(reg)
+                console.log(reg, 'reg', matchRes)
+                if (matchRes && matchRes.length >= 2 && matchRes[1].length >= 1) {
+                // matchRes = matchRes.filter(item => item && item.indexOf('\r') < 0)
+                  item[element.title] = matchRes[1]
+                  console.log(element.title, matchRes, 'hahah')
+                  break
+                } else {
+                  item[element.title] = ''
+                }
               }
-              console.log(element.title, element.vaule, 'hahah')
             }
           }
           xlsxArray.push(item)
